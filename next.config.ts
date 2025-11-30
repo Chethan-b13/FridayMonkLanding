d0cf9bb2f118d3346eb2096a0459193d12eeaa93
chethancheths <chethancheths13@gmail.com>
2025-12-01 01:20:42 +0530

    vercel deploy changes

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
-  basePath: "/FridayMonkLanding",
-  // Ensure static assets are prefixed with the repo name when deployed to GitHub Pages
-  assetPrefix: "/FridayMonkLanding",
-  // Export with trailing slashes so pages become directories with index.html
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
