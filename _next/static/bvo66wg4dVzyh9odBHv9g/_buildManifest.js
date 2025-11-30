2cfe4586ef232b3201ec640109a5d78c4a334f32
chethancheths <chethancheths13@gmail.com>
2025-12-01 01:04:45 +0530

    Updates

--- _next/static/bvo66wg4dVzyh9odBHv9g/_buildManifest.js
+++ _next/static/bvo66wg4dVzyh9odBHv9g/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/FridayMonkLanding//_next/:path+",
+        "destination": "/FridayMonkLanding/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
